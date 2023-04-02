a9dbcc8d81d1f33363be2642e2141b71e03380f9
Documenter.jl <documenter@juliadocs.github.io>
2023-04-02 11:04:56 +0000

    build based on 630cd55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MatterBot","category":"page"},{"location":"#MatterBot","page":"Home","title":"MatterBot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatterBot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MatterBot]","category":"page"}]
+}
